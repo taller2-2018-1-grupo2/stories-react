75345002985be17ef7ea80ddb64b8e914f5ca7ac
gmerino92 <merino.gonza@gmail.com>
2018-06-24 06:50:02 -0300

    Cleaner version.

--- src/components/ServerTable.js
+++ src/components/ServerTable.js
@@ -3,83 +3,25 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import axios from 'axios'
 
 const SHARED_SERVER_URI = "https://shared-server-stories.herokuapp.com/api"
-let mToken = null;
-
-const setToken = token => mToken = token;
-
-var servers = [];
-
-async function onBeforeSaveCell(row, cellName, cellValue) {
-    // You can do any validation on here for editing value,
-    // return false for reject the editing
-
-    console.log(row._rev);
-
-    let result = false;
-
-    if (cellName === "name") {
-        await axios({
-            method:'put',
-            data: {
-                name: cellValue,
-                _rev: servers[servers.findIndex(element => element.id === row.id)]._rev,
-                url: row.url
-            },
-            url: SHARED_SERVER_URI + '/servers/' + row.id,
-            headers: {'Authorization': 'Bearer ' + mToken}
-            })
-              .then(function(response) {
-                console.log(response)
-                const server = response.data.server.server;
-                servers[servers.findIndex(element => element.id === server.id)] = server;
-                result = true;
-              })
-              .catch(function (error) {
-                console.log(error);
-                result = false;
-              });
-    } else {
-        await axios({
-            method:'put',
-            data: {
-                url: cellValue,
-                _rev: servers[servers.findIndex(element => element.id === row.id)]._rev,
-                name: row.name
-            },
-            url: SHARED_SERVER_URI + '/servers/' + row.id,
-            headers: {'Authorization': 'Bearer ' + mToken}
-            })
-              .then(function(response) {
-                console.log(response)
-                const server = response.data.server.server;
-                servers[servers.findIndex(element => element.id === server.id)] = server;
-                result = true;
-              })
-              .catch(function (error) {
-                console.log(error);
-                result = false;
-              });
-    }
-
-    return result;
-}
-
-const selectRowProp = {
-    mode: 'checkbox'
-};
-
-const cellEditProp = {
-    mode: 'dbclick',
-    blurToSave: true,
-    beforeSaveCell: onBeforeSaveCell
-};
 
 
 export default class ServerTable extends Component {
 
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        servers: []
+    };
+
+    this.customConfirm = this.customConfirm.bind(this);
+    this.onAddRow = this.onAddRow.bind(this);
+    this.onBeforeSaveCell = this.onBeforeSaveCell.bind(this);
+  }
+
   async componentDidMount(){
     //console.log(this.props.childProps.token.token)
-    setToken(this.props.childProps.token.token);
+    const setServers = mServers => this.setState({servers: mServers});
 
     await axios({
       method:'get',
@@ -101,14 +43,93 @@ export default class ServerTable extends Component {
             }
           });
 
-          servers = response.data.servers;
+          setServers(response.data.servers);
         })
         .catch(function (error) {
           console.log(error);
         });
 
-    this.setState({flag: true});
   };
+
+  async onBeforeSaveCell(row, cellName, cellValue) {
+    // You can do any validation on here for editing value,
+    // return false for reject the editing
+
+    console.log(row._rev);
+    const setServers = mServers => this.setState({servers: mServers});
+
+    let result = false;
+
+    if (cellName === "name") {
+        await axios({
+            method:'put',
+            data: {
+                name: cellValue,
+                _rev: row._rev,
+                url: row.url
+            },
+            url: SHARED_SERVER_URI + '/servers/' + row.id,
+            headers: {'Authorization': 'Bearer ' + this.props.childProps.token.token}
+            })
+              .then(function(response) {
+                console.log(response)
+                result = true;
+              })
+              .catch(function (error) {
+                console.log(error);
+                result = false;
+              });
+    } else {
+        await axios({
+            method:'put',
+            data: {
+                url: cellValue,
+                _rev: row._rev,
+                name: row.name
+            },
+            url: SHARED_SERVER_URI + '/servers/' + row.id,
+            headers: {'Authorization': 'Bearer ' + this.props.childProps.token.token}
+            })
+              .then(function(response) {
+                console.log(response)
+                result = true;
+              })
+              .catch(function (error) {
+                console.log(error);
+                result = false;
+              });
+    }
+
+    if (result === true) {
+        await axios({
+            method:'get',
+            url: SHARED_SERVER_URI + '/servers',
+            headers: {'Authorization': 'Bearer ' + this.props.childProps.token.token}
+            })
+              .then(function(response) {
+                console.log(response)
+      
+                response.data.servers.forEach(server => {
+                  let date = new Date(server.createdTime);
+                  server.createdTime = date.toLocaleDateString();
+                });
+      
+                response.data.servers.forEach(server => {
+                  if (server.lastConnection !== "") {
+                      let date = new Date(server.lastConnection);
+                      server.lastConnection = date.toLocaleString();
+                  }
+                });
+      
+                setServers(response.data.servers);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+    }
+
+    return result;
+  }
 
   customConfirm(next, dropRowKeys) {
     console.log(dropRowKeys);
@@ -127,9 +148,19 @@ export default class ServerTable extends Component {
         noDataText: 'La información aun no ha cargado.'
     };
 
+    const selectRowProp = {
+        mode: 'checkbox'
+    };
+    
+    const cellEditProp = {
+        mode: 'dbclick',
+        blurToSave: true,
+        beforeSaveCell: this.onBeforeSaveCell
+    };
+
     return (
       <div>
-        <BootstrapTable ref='serverTable' data={ servers } insertRow={ true } deleteRow={ true } selectRow={ selectRowProp } options={ options }
+        <BootstrapTable ref='serverTable' data={ this.state.servers } insertRow={ true } deleteRow={ true } selectRow={ selectRowProp } options={ options }
                     headerStyle={ { background: '#f8f8f8' } } cellEdit={ cellEditProp }>
             <TableHeaderColumn dataField='id' editable={ false } isKey={ true } width='70'>ID</TableHeaderColumn>
             <TableHeaderColumn dataField='name' width='200'>Nombre</TableHeaderColumn>
